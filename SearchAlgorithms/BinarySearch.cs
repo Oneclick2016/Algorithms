46a993e031ff8f20dac21b6ca9484715c369be63
Oneclick2016 <fallenange.trololo@gmail.com>
2017-08-11 08:01:40 +0300

    Search

--- SearchAlgorithms/BinarySearch.cs
+++ SearchAlgorithms/BinarySearch.cs
@@ -1,49 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BinarySearch
+﻿class Search
 {
-    class Program
+    static int[] numbers = { 1, 5, 6, 11, 12, 17 };
+
+    static void Main(string[] args)
     {
-        static int[] numbers = { 1, 5, 6, 11, 12, 17 };
+        BSearch(numbers, 6);
+    }
 
-        static void Main(string[] args)
+    static void BSearch(int[] arr, int searchNumber)
+    {
+        int min = 0;
+        int max = arr.Length - 1;
+        int middle;
+
+        while (min <= max)
         {
-            BSearch(numbers, 6);
-        }
-
-        static void BSearch(int[] arr,int searchNumber)
-        {
-            int min = 0;
-            int max = arr.Length - 1;
-            int middle;
-
-            while (min <= max)
+            middle = (min + max) / 2;
+            if (searchNumber == arr[middle])
             {
-                middle = (min + max) / 2;
-                if (searchNumber == arr[middle])
-                {
-                    Console.WriteLine("Ваше число " + searchNumber + "\nНаходится под индексом " + middle + "\n");
-                    return;
-                }
-                else if (searchNumber > arr[middle])
-                {
-                    min = middle + 1;
-                }
-                else
-                {
-                    max = middle- 1;
-                }
-
+                Console.WriteLine("Ваше число " + searchNumber + "\nНаходится под индексом " + middle + "\n");
+                return;
             }
-            Console.WriteLine("Числа нет в массиве");
+            else if (searchNumber > arr[middle])
+            {
+                min = middle + 1;
+            }
+            else
+            {
+                max = middle - 1;
+            }
         }
-
-       
-
+        Console.WriteLine("Числа нет в массиве");
     }
 }
